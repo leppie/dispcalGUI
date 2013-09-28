b6bda334251d9bfb6eeef7ce3ec73e83730d1a34
Llewellyn Pritchard <xacc.ide@gmail.com>
2013-09-28 16:54:17 +0200

    Playing with 2407WFP combos

--- DELL 2407WFP 140cdm� 0.312699x 0.329001y sRGB M-S LabLUT/DELL 2407WFP 140cdm� 0.312699x 0.329001y sRGB M-S LabLUT.all.cmd
+++ DELL 2407WFP 140cdm� 0.312699x 0.329001y sRGB M-S LabLUT/DELL 2407WFP 140cdm� 0.312699x 0.329001y sRGB M-S LabLUT.all.cmd
@@ -2,13 +2,13 @@
 PATH C:\Users\leppie\Downloads\Argyll_V1.6.0\bin;%PATH%
 pushd "%~dp0"
 color 07
-dispread -v -d2 -c1 -yn "-P0.489417989418,0.487373737374,1.43661971831" -X CCFLFamily_07Feb11.ccss -k "2407WF~1.CAL" " 2407WFP 140cdm² 0.312699x 0.329001y sRGB M-S LabLUT 912 (R199 G206 B249 BR52)"
+dispread -v -d2 -c1 -yn "-P0.489417989418,0.487373737374,1.43661971831" -X CCFLFamily_07Feb11.ccss -k "DELL24~1.CAL" "DELL 2407WFP 140cdm² 0.312699x 0.329001y sRGB M-S LabLUT"
 set exitcode=%errorlevel%
 color
 popd
 if not %exitcode%==0 exit /B %exitcode%
 pushd "%~dp0"
-colprof -v -qh -al -C "No copyright. Created with dispcalGUI 0.0.0.0 and Argyll CMS 1.6.0" -M "DELL 2407WFP" -A Dell -D " 2407WFP 140cdm² 0.312699x 0.329001y sRGB M-S LabLUT 912 (R199 G206 B249 BR52)" " 2407WFP 140cdm² 0.312699x 0.329001y sRGB M-S LabLUT 912 (R199 G206 B249 BR52)"
+colprof -v -qh -al -C "No copyright. Created with dispcalGUI 0.0.0.0 and Argyll CMS 1.6.0" -M "DELL 2407WFP" -A Dell -D "DELL 2407WFP 140cdm² 0.312699x 0.329001y sRGB M-S LabLUT" "DELL 2407WFP 140cdm² 0.312699x 0.329001y sRGB M-S LabLUT"
 set exitcode=%errorlevel%
 popd
 if not %exitcode%==0 exit /B %exitcode%
--- DELL 2407WFP 140cdm� 0.312699x 0.329001y sRGB M-S LabLUT/DELL 2407WFP 140cdm� 0.312699x 0.329001y sRGB M-S LabLUT.dispcal.cmd
+++ DELL 2407WFP 140cdm� 0.312699x 0.329001y sRGB M-S LabLUT/DELL 2407WFP 140cdm� 0.312699x 0.329001y sRGB M-S LabLUT.dispcal.cmd
@@ -2,7 +2,7 @@
 PATH C:\Users\leppie\Downloads\Argyll_V1.6.0\bin;%PATH%
 pushd "%~dp0"
 color 07
-dispcal -v2 -d2 -c1 -yn "-P0.489417989418,0.487373737374,1.43661971831" -X CCFLFamily_07Feb11.ccss -qm "-w0.312699,0.329001" -b140.0 -gs -f1.0 -k0 -A4.0 " 2407WFP 140cdm² 0.312699x 0.329001y sRGB M-S LabLUT 912 (R199 G206 B249 BR52)"
+dispcal -v2 -d2 -c1 -yn "-P0.489417989418,0.487373737374,1.43661971831" -X CCFLFamily_07Feb11.ccss -qm "-w0.312699,0.329001" -b140.0 -gs -f1.0 -k0 -A4.0 "DELL 2407WFP 140cdm² 0.312699x 0.329001y sRGB M-S LabLUT"
 set exitcode=%errorlevel%
 color
 popd
